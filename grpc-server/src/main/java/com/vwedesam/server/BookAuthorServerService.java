56141ee218fcef48d8c248771db8b3d46c35ac56
Eshemeidomi Samuel Oghenevwede <eshemiedomisamuelb@gmail.com>
2023-07-26 11:56:05 +0100

    [server]: updated BookAuthorServerService

--- grpc-server/src/main/java/com/vwedesam/server/BookAuthorServerService.java
+++ grpc-server/src/main/java/com/vwedesam/server/BookAuthorServerService.java
@@ -4,23 +4,35 @@ import com.vwedesam.grpc.Author;
 import com.vwedesam.grpc.BookAuthorServiceGrpc;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
 @GrpcService
 public class BookAuthorServerService extends BookAuthorServiceGrpc.BookAuthorServiceImplBase {
 
+    private static Logger log = LoggerFactory.getLogger(BookAuthorServerService.class);
+
     @Override
     public void getAuthor(Author request, StreamObserver<Author> responseObserver) {
 
-        String authorId = String.valueOf(request.getAuthorId());
+        int authorId = request.getAuthorId();
+
+        log.info(String.valueOf(authorId));
 
         Optional<Author> author = TempDB.getAuthorsFromTempDb()
                     .stream()
-                .filter(author1 -> String.valueOf(author1.getAuthorId()) == authorId)
+                .filter(author1 -> author1.getAuthorId() == authorId)
                         .findFirst();
 
-        responseObserver.onNext(author.get());
+        Author author1 = Author.newBuilder().build();
+
+        if(author.isPresent()){
+            author1 = author.get();
+        }
+
+        responseObserver.onNext(author1);
         responseObserver.onCompleted();
     }
 
